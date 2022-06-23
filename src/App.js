91da108839e57b41f10b138712d686571877d61d
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-06-23 19:10:44 +0100

    women

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header'
+import Banner from './components/banner/Banner'
+import Article from './components/article/Article'
+import BlogArticle from './components/blogArticle/BlogArticle'
+import Quote from './components/quote/Quote'
+import LookBook from './components/lookBook/LookBook'
+import Collections from './components/collections/Collections'
+import Promo from './components/promo/Promo'
+import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Banner />
+      <Article />
+      <BlogArticle />
+      <Quote />
+      <LookBook />
+      <Collections />
+      <Promo />
+      <Footer />
     </div>
   );
 }
